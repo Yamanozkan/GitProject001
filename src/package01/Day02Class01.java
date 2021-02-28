6e0a76a6d1ca4036de0a80ca85fed98c33fee27f
yamanozkan <yamanozkan91@gmail.com>
2021-02-28 18:58:05 +0300

    third commit

--- src/package01/Day02Class01.java
+++ src/package01/Day02Class01.java
@@ -1,5 +1,20 @@
 package package01;
 
 public class Day02Class01 {
+	public static void main(String[] args) {
+		
+		/*
+		 Task:
+			Create a new project called GitProject001
+			on your local editor eclipse/ VS Code
+			Then you will initialiaze your project with git
+			you make your first commit
+			Then you create a public repository on GitHub
+			Finally you push all project to GitHub
+		 */
+		
+		// hello everyone
+ 		
+	}
 
 }
